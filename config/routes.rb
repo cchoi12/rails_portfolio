156b3338b2f24725f2d552c5ec172651a7a5a40a
Christopher Choi <cdddchris@gmail.com>
2017-10-03 23:25:35 -0700

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
 
-  get 'pages/home'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  # resources automatically creates the most
-  # common routes from the controller.
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
