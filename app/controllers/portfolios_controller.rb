2ac3f8cad8f49d0bb6a1cd25e118030bf420ca03
Christopher Choi <cdddchris@gmail.com>
2017-10-06 15:41:15 -0700

    add concerns for image data

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,10 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.angular_portfolio_items
   end
 
+  def rails_portfolio
+    @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
+  end
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
