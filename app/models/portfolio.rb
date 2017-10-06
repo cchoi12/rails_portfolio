0d644ecabba4d3ff6c8297efa466c670c165d128
Christopher Choi <cdddchris@gmail.com>
2017-10-06 15:23:40 -0700

    set defaults for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,8 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body
 
+  after_initialize :set_defaults
+
   # This is the samething as a scope like line #9.
   # def self.angular
   #   where(subtitle: 'Angular')
@@ -8,4 +10,15 @@ class Portfolio < ApplicationRecord
 
   scope :angular_portfolio_items, -> { where(subtitle: 'Angular') }
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+  def set_defaults
+    self.main_image ||= 'http://via.placeholder.com/600x400'
+    self.thumb_image ||= 'http://via.placeholder.com/350x200'
+  end
 end
+
+# What does self.main_image ||= 'http://via.placeholder.com/600x400'
+# or the '||=' mean?
+# if self.main_image == nil
+#   self.main_image = 'http://via.placeholder.com/600x400'
+# end
