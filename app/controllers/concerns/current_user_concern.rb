0d68f1c8ac2f73313f47418b6bbb2dcf6274443b
Christopher Choi <cdddchris@gmail.com>
2017-10-09 22:59:15 -0700

    add some notes

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -5,6 +5,9 @@ module CurrentUserConcern
     super || guest_user
   end
 
+  # By creating a struct, we can use it almost like a dumby
+  # we use super || to allow the original functionality to work
+  # but also to add an exception
   def guest_user
     OpenStruct.new(name: 'Guest User',
                    first_name: 'Guest',
