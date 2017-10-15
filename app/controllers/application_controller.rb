c23f428d2bfdfc4b6714f4e04836e46f17965f7d
Christopher Choi <cdddchris@gmail.com>
2017-10-15 14:00:42 -0700

    extract before_action into view helper, deals with view not data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,4 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-
-  before_action :set_copyright
-
-  def set_copyright
-    @copyright = AvaViewTool::Renderer.copyright 'Christopher Choi', 'All rights reserved'
-  end
 end
