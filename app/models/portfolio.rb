1b26577caec4c0fe6d2f74e17fdc3ce6439dfeb2
Christopher Choi <cdddchris@gmail.com>
2017-10-26 00:51:36 -0700

    add placeholder, remove unused code

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,12 +1,10 @@
 class Portfolio < ApplicationRecord
   include Placeholder
 
-  after_initialize :set_defaults
-
   has_many :technologies
 
-  has_attached_file :show_image, styles: { medium: "300x300>", thumb: "200x75>" }, default_url: "/images/:style/missing.png"
-  has_attached_file :thumbnail_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :show_image, styles: { medium: "300x300>", thumb: "200x75>" }, default_url: 'http://via.placeholder.com/600x400'
+  has_attached_file :thumbnail_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: 'http://via.placeholder.com/200x175'
 
   accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
 
@@ -17,11 +15,6 @@ class Portfolio < ApplicationRecord
   scope :angular_portfolio_items, -> { where(subtitle: 'Angular') }
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
   scope :by_position, -> { order('position ASC') }
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 end
 
 # What does self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
