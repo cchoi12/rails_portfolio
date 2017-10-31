d47625aaf643e307c1c1d233653386c2009cedd3
Christopher Choi <cdddchris@gmail.com>
2017-10-30 17:33:29 -0700

    create comment broadcast, configure websocket route; updating the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   # path: '', path_names: { current_name: 'new name' } changes the routes names
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
@@ -20,6 +19,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
+  mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
 end
